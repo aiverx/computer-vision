994d85d88904e68176431c582e7e7b65eab437b2
aiverx <aiverx@outlook.com>
2023-03-26 17:52:40 +0500

    Check OpenCV version

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Check OpenCV Version
+
+#include "main.h"
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	cout << "OpenCV version: " << CV_VERSION << endl;
+	return 0;
+};
